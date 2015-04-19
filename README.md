f925372bc6e487480b487dd339d31e2042c33e62
leilei- <leilei-@users.noreply.github.com>
2015-04-19 03:40:55 -0400

    Initial commit of the UI so far

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 UI3
 ===
 
-UI scripts and assets for OA3
+UI scripts and assets for OA3 as it exists so far.
+
+
+
+This requires a compiled binary of leixperimental's cgame and ui modules to use (due to many extensions).
---
+++ ui/sp/ladder_tourney.h
@@ -0,0 +1,27 @@
+
+
+#define SPCUR_LADDERNAME	"OneOnOne"
+#define SPCUR_MENUNAME		"singleplayercampaign_tourney"
+
+// For FFA...
+
+// Internal workings that don't really need to be modified
+
+
+#define SPCUR_MAP1_UNLOCKCVAR 	"ui_sp_unlock_31"
+#define SPCUR_MAP1NUM		32
+#define SPCUR_MAP2_UNLOCKCVAR 	"ui_sp_unlock_32"
+#define SPCUR_MAP2NUM		33
+#define SPCUR_MAP3_UNLOCKCVAR 	"ui_sp_unlock_33"
+#define SPCUR_MAP3NUM		34
+#define SPCUR_MAP4_UNLOCKCVAR 	"ui_sp_unlock_34"
+#define SPCUR_MAP4NUM		35
+#define SPCUR_MAP5_UNLOCKCVAR 	"ui_sp_unlock_35"
+#define SPCUR_MAP5NUM		36
+#define SPCUR_MAP6_UNLOCKCVAR 	"ui_sp_unlock_36"
+#define SPCUR_MAP6NUM		37
+#define SPCUR_MAP7_UNLOCKCVAR 	"ui_sp_unlock_37"
+#define SPCUR_MAP7NUM		38
+#define SPCUR_MAP8_UNLOCKCVAR 	"ui_sp_unlock_38"
+#define SPCUR_MAP8NUM		39
+#define SPCUR_MAP9_UNLOCKCVAR 	"ui_sp_unlock_39"
